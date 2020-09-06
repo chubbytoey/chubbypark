ffffdbcf3843175e44853e27c06eca70e140aa4c
Supphawit-J <ideajeng2543@gmail.com>
2020-09-06 13:14:40 +0700

    add category model

---
+++ app/Models/Category.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Category extends Model {
+    static get primaryKey(){
+        return 'category_id'
+
+    }
+    parkinglot(){
+        return this.belongsToMany('App/Models/Parkinglot')
+    }
+}
+
+module.exports = Category
