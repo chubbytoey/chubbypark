b956f721f1b7002de0dd85d1db9c17e732b9e1aa
Supphawit-J <ideajeng2543@gmail.com>
2020-09-06 13:15:29 +0700

    add location model

---
+++ app/Models/Location.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Location extends Model {
+    static get primaryKey(){
+        return 'location_id'
+    }
+    parkinglot(){
+        return this.hasMany('App/Models/Parkinglot')
+    }
+}
+
+module.exports = Location
