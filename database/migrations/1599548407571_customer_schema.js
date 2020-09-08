beba2e8b630eb28454a81d53e89e67e3adbb801d
chubbytoey <toey.sasikrang@gmail.com>
2020-09-08 14:21:17 +0700

    add customer migration

---
+++ database/migrations/1599548407571_customer_schema.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CustomerSchema extends Schema {
+  up () {
+    this.create('customers', (table) => {
+      table.increments('customer_id')
+      table.integer('account_id').unsigned()
+      table.string('first_name',120).notNullable()
+      table.string('last_name',120).notNullable()
+      table.integer('age',3).notNullable()
+      table.string('gender',10).notNullable()
+      table.integer('user_rate',1).default(0)
+      table.string('previllage',20)
+      table.integer('reservation').default(0)
+      table.integer('cancle').default(0)
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('customers')
+  }
+}
+
+module.exports = CustomerSchema
