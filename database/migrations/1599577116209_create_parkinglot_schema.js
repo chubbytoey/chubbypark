6418a71bfe7c60d54a3291463f0dbe6df4e86c5c
Supphawit-J <ideajeng2543@gmail.com>
2020-09-09 01:16:45 +0700

    add  parkinglot tablee

---
+++ database/migrations/1599577116209_create_parkinglot_schema.js
@@ -0,0 +1,65 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CreateParkinglotSchema extends Schema {
+  up () {
+    this.create('parkinglots', (table) => {
+      table
+        .increments('code')
+      table
+        .string('lot_status',12)
+        .notNullable()
+      table
+        .time('checkin')
+        .notNullable()
+        .default(this.fn.now())
+      table
+        .time('checkout')
+      table
+        .smallint('hour',2 )
+      table
+        .decimal('price')
+        .notNullable()
+      table
+        .integer('category_id')
+        .unsigned()
+        .notNullable()
+      table
+        .integer('location_id')
+        .unsigned()
+        .notNullable()
+      table
+        .integer('customer_id')
+        .unsigned()
+        .notNullable()
+
+      table.timestamps()
+
+      table
+        .foreign('category_id')
+        .references('categories.category_id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      table
+        .foreign('loacation_id')
+        .references('locations.location_id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      table
+        .foreign('customer_id')
+        .references('customers.customer_id')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+
+
+    })
+  }
+
+  down () {
+    this.drop('parkinglots')
+  }
+}
+
+module.exports = CreateParkinglotSchema
