998036d2755a4d7268ef877c0328ff41d3f7c1ec
Supphawit-J <ideajeng2543@gmail.com>
2020-09-10 05:56:45 +0700

    add location validator

---
+++ service/LocationValidator.js
@@ -0,0 +1,24 @@
+const Validator = use("Validator")
+module.exports = async function locationValidator (data) {
+    if (typeof data !== 'object') throw new Error()
+
+    const {location_name,price_rate,category_id} = data
+
+
+
+    const rules = {
+        location_name:'required',
+        price_rate:'required',
+        category_id:'required'
+    }
+
+
+const validation =await Validator.validateAll ({
+    location_name,price_rate,category_id
+},rules)
+
+return{
+    error: validation.messages()
+    }
+    
+}
