d2505bf12571983808fb8af0ed27a03e3b967bf9
Supphawit-J <ideajeng2543@gmail.com>
2020-09-11 21:13:14 +0700

    edit factory

--- database/factory.js
+++ database/factory.js
@@ -12,6 +12,35 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+// const Location= use('App/Models/Location')
+// const maximumLocation = Location.query("SELECT COUNT(location_id) FROM locations ")
+
+
+Factory.blueprint('App/Models/Category', (faker) =>{
+    return{
+        type: faker.string(),
+        hour: faker.hour({min:1,max:3})
+    }
+})
+
+Factory.blueprint('App/Models/Location', (faker) =>{
+    return{
+        location_name: faker.string(),
+        price_rate: faker.floating({min:0,max:100,fixed:2}),
+        category_id: faker.integer({min:1,max:3})
+    }
+})
+
+Factory.blueprint('App/Models/Parkinglot', (faker) =>{
+    return{
+        location_id: faker.integer({min:1,max:3}),
+        category_id: faker.integer({min:1,max:3})
+    }
+})
+
+
 // const Factory = use('Factory')
 
 // Factory.blueprint('App/Models/User', (faker) => {
