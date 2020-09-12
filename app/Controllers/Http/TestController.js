79ec5aaf894abe911abd24b11a1683c4e6eb5aeb
chubbytoey <toey.sasikrang@gmail.com>
2020-09-13 04:48:11 +0700

    add check status (mockup)

--- app/Controllers/Http/TestController.js
+++ app/Controllers/Http/TestController.js
@@ -1,40 +1,22 @@
 'use strict'
 
-const Account = use('App/Models/Account');
+const Database = use('Database')
 
 class TestController {
-    async register({request, auth, response}) {
-        const username = request.input("username")
-        const email = request.input("email")
-        const password = request.input("password")
-
-        let user = new User()
-        user.username = username
-        user.email = email
-        user.password = password
-
-        user = await user.save()
-        //let accessToken = await auth.generate(user)
-        return "register success"
-    }
-
-    async login({request, auth, response}) {
-        const {username} = request.body
-        const {password} = request.body
-
-        try {
-          if (await auth.attempt(username, password)) {
-            let user = await Account.findBy('username', username)
-
-            let accessToken = await auth.generate(user)
-            return response.json({"user":user, "access_token": accessToken})
-          }
-
+    async checkLogin({auth}) {
+      try {
+        await auth.check()
+        const user = await auth.getUser()
+        // const joinUser = await Database.table('customers').where('account_id',user.account_id)
+        // console.log(joinUser[0].first_name)
+        if (user.status == 'customer') {
+          return 'user'
+        } else {
+          return 'admin'
         }
-        catch (e) {
-          return response.json({message: 'You first need to register!'})
-          //return e
-        }
+      } catch {
+        return 'not'
+      }
     }
 }
 
