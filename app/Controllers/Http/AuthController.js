79367393ae20aed4f81fbdd3e89122db724f158a
chubbytoey <toey.sasikrang@gmail.com>
2020-09-13 02:15:27 +0700

    add mockup logout and getUser

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,23 +1,46 @@
 'use strict'
 const Hash = use('Hash')
-const User = use('App/Models/User')
+const Account = use('App/Models/Account')
+const Database = use('Database')
 
 class AuthController {
     async login({request , auth}) {
         const {username} = request.body
         const {password} = request.body
-        const user = await User.findBy('username',username)
+        const user = await Account.findBy('username',username)
         const hashPassword = await Hash.verify(password,user.password)
         try{
-            if(hashPassword){
+            if(hashPassword && user.password !== null){
                 const accessToken = await auth.generate(user)
-                return {status:'Login success' , access:accessToken}
+                return {status:'Login success' , access:accessToken , username : user.username}
             }
         }
         catch{
             return 'Not login'
         }
     }
+    async getUser({auth}) {
+        try {
+            return await auth.getUser()
+          } catch (error) {
+            return 'Missing or invalid jwt token'
+          }
+    }
+    // async logout({auth}) {
+    //     await auth
+    //         .authenticator('jwt')
+    //         .revokeTokens()
+        
+    // }
+        async logout ({ auth }) {
+          const user = auth.current.user
+          const token = auth.getAuthHeader()
+      
+          await user
+            .tokens()
+            .where('token', token)
+            .update({ is_revoked: true })
+        }
 }
 
 module.exports = AuthController
