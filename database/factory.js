03c48d33ddb4d425a1bb646cab6124fcc913c89f
Supphawit-J <ideajeng2543@gmail.com>
2020-09-14 01:03:32 +0700

    edit factory

--- database/factory.js
+++ database/factory.js
@@ -13,9 +13,13 @@
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
-
+const Database = use('Database')
 // const Location= use('App/Models/Location')
 // const maximumLocation = Location.query("SELECT COUNT(location_id) FROM locations ")
+
+const countLocation = await Database.from('locations').getCount()
+const countCustomer = await Database.from('customers').getCount()
+const countCategory = await Database.from('categories').getCount()
 
 
 Factory.blueprint('App/Models/Category', (faker) =>{
@@ -28,17 +32,10 @@ Factory.blueprint('App/Models/Category', (faker) =>{
 Factory.blueprint('App/Models/Location', (faker) =>{
     return{
         location_name: faker.word(),
-        price_rate: faker.floating({min:0,max:100,fixed:2}),
-        category_id: faker.integer({min:1,max:3})
+        price_rate: faker.floating({min:0,max:100,fixed:2})
     }
 })
 
-Factory.blueprint('App/Models/Parkinglot', (faker) =>{
-    return{
-        location_id: faker.integer({min:1,max:3}),
-        category_id: faker.integer({min:1,max:3})
-    }
-})
 
 Factory.blueprint('App/Models/Account', (faker) =>{
     return{
@@ -53,6 +50,19 @@ Factory.blueprint('App/Models/Customer', (faker) =>{
         last_name: faker.word(),
         age : faker.integer({min:1,max:3}),
         gender : faker.gender()
+    }
+})
+
+
+Factory.blueprint('App/Models/Parkinglot', (faker) =>{
+    return{
+        lot_name: faker.word(),
+        lot_status : faker.word(),
+        reserve_time : faker.timestamp(),
+        checkin: faker.timestamp(),
+        customer: faker.integer({min:1,max:countCustomer}),
+        location_id: faker.integer({min:1,max:countLocation}),
+        category_id: faker.integer({min:1,max:countCategory})
     }
 })
 
