4cc954dd5fd374d4b0b92c82a7267b1d0f3ea3e9
Supphawit-J <ideajeng2543@gmail.com>
2020-09-13 22:38:42 +0700

    edit reserve controller

--- app/Controllers/Http/ReserveController.js
+++ app/Controllers/Http/ReserveController.js
@@ -1,10 +1,9 @@
 'use strict'
 
-const Database = use('Database')
 const Location = use('App/Models/Location')
 const ParkingLot = use('App/Models/Parkinglot')
 const Customer =use('App/Models/Customer')
-
+const Category =use('App/Models/Category')
 
 
 function numberTypeParamValidator(number){
@@ -40,6 +39,7 @@ class ReserveController {
         }
 
         try {
+            await auth.check()
             const token = await auth.getUser()
 
             const userData = await Customer.findBy('account_id',token.account_id)
@@ -80,14 +80,171 @@ class ReserveController {
             return await ParkingLot.query().where({location_id:location_id}).fetch()
           }
 
+    }
+
+    async reserve ({request,auth}){
+        const {body,params} = request
+        const {lot_name} = body
+        const {location_id} = params
+       
+  
+        try {
+            await auth.check()
+            const token = await auth.getUser()
+
+            const userData = await Customer.findBy('account_id',token.account_id)
+            
+            const lotReserve = await ParkingLot.findBy({location_id:location_id,lot_name:lot_name})
+
+            let date = new Date();
+            let time = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds(); 
+
+            if(lotReserve.lot_status, "availble"){
+                lotReserve.merge({
+                    lot_status:"unavailable",
+                    customer_id:userData.customer_id,
+                    reserve_time:time})
+                await lotReserve.save() 
+
+                userData.merge({
+                    reservation: userData.reservation+1,
+                })
+                await userData.save()
+
+                return 'success'
+            }else{
+                return 'this lot is unavailable'
+            }
 
 
-    
+
           
-        
+          } catch (error) {
+
+            return 'please login first'
+          }
 
     }
 
+    async checkin ({auth}){
+
+        try {
+            await auth.check()
+            const token = await auth.getUser()
+
+            const userData = await Customer.findBy('account_id',token.account_id)
+            
+            const lotData = await ParkingLot.findBy('customer_id',userData.customer_id)
+            
+            
+            
+            let date = new Date();
+            let time = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+
+            lotData.merge({checkin:time})
+            await lotData.save()
+            
+            return 'check in'
+
+
+          } catch (error) {
+
+            return 'you have not reserved any lot '
+            
+          }
+    }
+
+    async checkout ({auth}){
+
+        try {
+            await auth.check()
+            const token = await auth.getUser()
+
+            const userData = await Customer.findBy('account_id',token.account_id)
+            
+            const lotData = await ParkingLot.findBy('customer_id',userData.customer_id)
+            
+            const categoryData = await Category.findBy('category_id',lotData.category_id)
+
+            const locationData = await Location.findBy('location_id',lotData.location_id) 
+
+            let date = new Date();
+            let time = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds(); 
+            let checkOut = new Date(date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate()+" "+time)
+            let checkIn = new Date(date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate()+" "+lotData.checkin)
+
+            let duration = Date.parse(checkOut) - Date.parse(checkIn);
+            
+            let second = Math.floor(duration/1000);
+            let minutes =Math.floor(second/60);
+            second = second%60;
+            let hour = Math.floor(minutes/60);
+            minutes =minutes%60;
+            let useHour = hour+":"+minutes+":"+second
+
+            let paidHour = parseInt(Math.ceil(duration/3600000))
+
+            if (paidHour>categoryData.free_hour){
+
+               let price =  paidHour*locationData.price_rate
+
+
+                userData.merge({coin:userData.coin - price})
+               
+                lotData.merge({lot_status:"available",customer_id:null,reserve_time:null,checkin:null})
+                await lotData.save()
+
+                return 'Check out success , use time : '+useHour+' price : '+price+' remain coin : '+ userData.coin
+            }else{
+
+                return 'Check out success , use time : '+useHour+' price : 0'
+            }
+
+
+          } catch (error) {
+
+            return 'you have not reserved any lot yet '
+            
+          }
+
+
+    }
+
+    async cancel ({auth}){
+
+        try {
+            await auth.check()
+            const token = await auth.getUser()
+
+            const userData = await Customer.findBy('account_id',token.account_id)
+            
+            const lotData = await ParkingLot.findBy('customer_id',userData.customer_id)
+            
+            lotData.merge({
+                lot_status:"available",
+                customer_id:null,
+                reserve_time:null
+            })
+            await lotData.save()
+            
+            userData.merge({
+                cancel: userData.cancel+1,
+            })
+            await userData.save()
+          
+            
+            return 'cancelled'
+
+
+          } catch (error) {
+
+            return 'you have not reserved any lot '
+            
+          }
+
+
+
+    }
 
 }
 
