ae8bf52ce33d11b4aad327b1e7b7fe80136f3739
chubbytoey <toey.sasikrang@gmail.com>
2020-09-15 05:19:42 +0700

    edit LocationValidator

--- service/LocationValidator.js
+++ service/LocationValidator.js
@@ -2,15 +2,14 @@ const Validator = use("Validator")
 module.exports = async function locationValidator(data) {
     if (typeof data !== 'object') throw new Error()
 
-    const { location_name, price_rate, category_id } = data
+    const { location_name, price_rate } = data
 
     const rules = {
         location_name: 'required | string | max:100 |unique:Locations,location_name',
-        price_rate: 'required | number',
-        category_id: 'required | integer'
+        price_rate: 'required | number'
     }
 
-    const validation = await Validator.validateAll({ location_name, price_rate, category_id }, rules)
+    const validation = await Validator.validateAll({ location_name, price_rate }, rules)
 
     return {
         error: validation.messages() || undefined
