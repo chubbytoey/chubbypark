72c672382c14bf87bfdb13520a518b2bfda00671
chubbytoey <toey.sasikrang@gmail.com>
2020-09-14 21:43:06 +0700

    edit account-validator-test

--- test/unit/account-validator.spec.js
+++ test/unit/account-validator.spec.js
@@ -5,30 +5,30 @@ const accountValidator = require('../../service/AccountValidator')
 
 test('should return undefined error when input correct data', async ({ assert }) => {
   const validatedData = await accountValidator({
-    username : "sarang",
-    password : "12345678"
+    username: "sarang",
+    password: "12345678"
   })
-  assert.equal(validatedData.error,undefined)
+  assert.equal(validatedData.error, undefined)
 })
 
 test('should return error if input incorrect data', async ({ assert }) => {
   const validatedData = await accountValidator({
-    username : "chubbytoey",
-    password : "toey"
+    username: "chubbytoey",
+    password: "toey"
   })
   assert.isArray(validatedData.error)
 })
 test('should return only one error if input incorrect password', async ({ assert }) => {
   const validatedData = await accountValidator({
-    username : "sayhi",
-    password : "1"
+    username: "sayhi",
+    password: "1"
   })
-  assert.equal(validatedData.error.length , 1)
+  assert.equal(validatedData.error.length, 1)
 })
-test('should return error if input not unique username' , async ({assert}) => {
+test('should return error if input not unique username', async ({ assert }) => {
   const validatedData = await accountValidator({
     username: "chubbytoey",
-    password : "12345678"
+    password: "12345678"
   })
   assert.isOk(validatedData.error)
 })
