d494f2b7e58e784b7b7d4c39367f26e5313b297b
chubbytoey <toey.sasikrang@gmail.com>
2020-09-15 02:35:42 +0700

    edit categoryController to access authorised only

--- app/Controllers/Http/CategoryController.js
+++ app/Controllers/Http/CategoryController.js
@@ -5,83 +5,123 @@ const Category = use('App/Models/Category')
 const CategoryValidator = require("../../../service/CategoryValidator")
 const CategoryUtil = require("../../../util/categoryUtil")
 
-function numberTypeParamValidator(number){
-    if(Number.isNaN(parseInt(number)))
+function numberTypeParamValidator(number) {
+    if (Number.isNaN(parseInt(number)))
         return { error: `param: ${number} is not supported, please use number type param instead.` }
     return {}
 }
 
+
 class CategoryController {
 
-    async index({request}){
-        
-        const {references = undefined} =request.qs
-        const categoryUtil = new CategoryUtil(Category)
-        const categories =  await categoryUtil.getAll(references)
-        
-        return {
-            status: 200,
-            error:undefined,
-            data: categories
+    async index({ request, auth }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            if (user.status == 'customer') {
+                return 'only admin can access the information'
+            } else {
+                const { references = undefined } = request.qs
+                const categoryUtil = new CategoryUtil(Category)
+                const categories = await categoryUtil.getAll(references)
+
+                return {
+                    status: 200,
+                    error: undefined,
+                    data: categories
+                }
+            }
+        } catch {
+            return 'only admin can acces the information'
         }
     }
-    async show({request}){
-        const {id} = request.params
 
-        const validatedValue = numberTypeParamValidator(id)
-        if(validatedValue.error)
-            return {status:500,error:validatedValue.error,data: undefined}
-            
-        const {references = undefined} =request.qs
-        const categoryUtil = new CategoryUtil(Category)
-        const category =  await categoryUtil.getById(id,references)
+    async show({ request, auth }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            if (user.status == 'customer') {
+                return 'only admin can access the information'
+            } else {
+                const { id } = request.params
 
-        return {
-            status:200,
-            error:undefined,
-            data: category || {}
+                const validatedValue = numberTypeParamValidator(id)
+                if (validatedValue.error)
+                    return { status: 500, error: validatedValue.error, data: undefined }
+
+                const { references = undefined } = request.qs
+                const categoryUtil = new CategoryUtil(Category)
+                const category = await categoryUtil.getById(id, references)
+
+                return {
+                    status: 200,
+                    error: undefined,
+                    data: category || {}
+                }
+            }
+        }
+        catch{
+            return 'only admin ca acces the information'
         }
     }
-    async store ({request}){
-        const {type,free_hour} = request.body
-        const validatedData = await CategoryValidator(request.body)
+    async store({ request, auth }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            if (user.status == 'customer') {
+                return 'only admin can access the information'
+            } else {
+                const { type, free_hour } = request.body
+                const validatedData = await CategoryValidator(request.body)
 
-        if (validatedData.error)
-            return{status: 422,error: validatedData.error,data: undefined}
-        
-       
-        const {references = undefined} =request.qs
-        const categoryUtil = new CategoryUtil(Category)
-        const category =  await categoryUtil.create({type,free_hour},references)
+                if (validatedData.error)
+                    return { status: 422, error: validatedData.error, data: undefined }
 
-        
-        await category.save()
-
-
+                const { references = undefined } = request.qs
+                const categoryUtil = new CategoryUtil(Category)
+                const category = await categoryUtil.create({ type, free_hour }, references)
+                await category.save()
+            }
+        } catch {
+            return 'only admin can access the information'
+        }
     }
-    async update ({request}){
-        const {id} = request.params
-        const {type} = request.body
-        const {free_hour} = request.body
-        const categoryUtil = new CategoryUtil(Category)
-        const categories = await categoryUtil.updateCategory(id,type,free_hour)
-        return {status:200 , error:undefined , data:categories}
-        // const category = await Category.find(id)
+    async update({ request, auth }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            if (user.status == 'customer') {
+                return 'only admin can see the information'
+            } else {
+                const { id } = request.params
+                const { type } = request.body
+                const { free_hour } = request.body
+                const categoryUtil = new CategoryUtil(Category)
+                const categories = await categoryUtil.updateCategory(id, type, free_hour)
 
-        // category.merge({type:type,hour:hour})
-        //  await category.save()
-
+                return { status: 200, error: undefined, data: categories }
+            }
+        } catch{
+            return 'only admin can access the information'
+        }
     }
-    async destroy ({request}){
-        const {id} = request.params
-        const categoryUtil = new CategoryUtil(Category)
-        const categories = await categoryUtil.deleteCategory(id)
-        return {status:200 , error:undefined , data:categories.message}
-        // const category = await Category.find(id)
-        // await category.delete()
+    async destroy({ request, auth }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            if (user.status == 'customer') {
+                return 'only admin can see the information'
+            } else {
+                const { id } = request.params
+                const categoryUtil = new CategoryUtil(Category)
+                const categories = await categoryUtil.deleteCategory(id)
+
+                return { status: 200, error: undefined, data: categories.message }
+            }
+        } catch {
+            return 'only admin can access the information'
+        }
     }
-
-
 }
 
 module.exports = CategoryController
