f17f37ba3a766743e64f57f9976bb462335d20a3
chubbytoey <toey.sasikrang@gmail.com>
2020-09-15 02:41:05 +0700

    edit LocationController to access authorised only

--- app/Controllers/Http/LocationController.js
+++ app/Controllers/Http/LocationController.js
@@ -1,97 +1,120 @@
 'use strict'
 
 
-const Location= use('App/Models/Location')
+const Location = use('App/Models/Location')
 const LocationValidator = require("../../../service/LocationValidator")
-const LocationUtil =require("../../../util/locationUtil")
+const LocationUtil = require("../../../util/locationUtil")
 
 
-function numberTypeParamValidator(number){
-    if(Number.isNaN(parseInt(number)))
+function numberTypeParamValidator(number) {
+    if (Number.isNaN(parseInt(number)))
         return { error: `param: ${number} is not supported, please use number type param instead.` }
     return {}
 }
 
 class LocationController {
 
-    async index({request}){
+    async index({ request }) {
 
-        const {references = undefined} =request.qs
+        const { references = undefined } = request.qs
         const locationUtil = new LocationUtil(Location)
-        const locations =  await locationUtil.getAll(references)
-        
+        const locations = await locationUtil.getAll(references)
+
         return {
             status: 200,
-            error:undefined,
+            error: undefined,
             data: locations
         }
     }
 
-    async show({request}){
-        const {id} = request.params
+    async show({ request }) {
+        const { id } = request.params
 
         const validatedValue = numberTypeParamValidator(id)
-        if(validatedValue.error)
+        if (validatedValue.error)
             return {
-                status:500,
-                error:validatedValue.error,
+                status: 500,
+                error: validatedValue.error,
                 data: undefined
             }
 
-        const {references = undefined} =request.qs
+        const { references = undefined } = request.qs
         const locationUtil = new LocationUtil(Location)
-        const location =  await locationUtil.getById(id,references)
+        const location = await locationUtil.getById(id, references)
 
         return {
-            status:200,
-            error:undefined,
+            status: 200,
+            error: undefined,
             data: location || {}
         }
     }
 
-    async store ({request}){
-        const {location_name,price_rate,category_id} = request.body
-        const validatedData = await LocationValidator(request.body)
+    async store({ request }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            if (user.status == 'customer') {
+                return 'only admin can access the information'
+            } else {
+                const { location_name, price_rate, category_id } = request.body
+                const validatedData = await LocationValidator(request.body)
 
-        if (validatedData.error)
-            return{
-                status: 422,
-                error: validatedData.error,
-                data: undefined
+                if (validatedData.error)
+                    return {
+                        status: 422,
+                        error: validatedData.error,
+                        data: undefined
+                    }
+
+                const { references = undefined } = request.qs
+                const locationUtil = new LocationUtil(Location)
+                const location = await locationUtil.create({ location_name, price_rate, category_id }, references)
+
+                await location.save()
             }
-        
-        const {references = undefined} =request.qs
-        const locationUtil = new LocationUtil(Location)
-        const location =  await locationUtil.create({location_name,price_rate,category_id},references)
-        
-        await location.save()
+        } catch{
+            return 'only admin can acces the information'
 
+        }
 
     }
 
-    async update ({request}){
-        const {body, params} = request
-        const {id} = params
-        const {location_name,price_rate} = body
-        const locationUtil = new LocationUtil(Location)
-        const locations = await locationUtil.updateLocation(id,location_name,price_rate)
-        return {status:200 , error:undefined , data:locations}
-        // const location = await Location.find(id)
-
-        // location.merge({location_name:location_name,price_rate:price_rate,category_id:category_id})
-        //  await location.save()
+    async update({ request }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            if (user.status == 'customer') {
+                return 'only admin can access the information'
+            } else {
+                const { body, params } = request
+                const { id } = params
+                const { location_name, price_rate } = body
+                const locationUtil = new LocationUtil(Location)
+                const locations = await locationUtil.updateLocation(id, location_name, price_rate)
+                return { status: 200, error: undefined, data: locations }
+            }
+        } catch{
+            return 'only admin can acces the information'
+        }
 
     }
 
-    async destroy ({request}){
-        const {id} = request.params
-        const locationUtil = new LocationUtil(Location)
-        const locations = await locationUtil.deleteLocation(id)
-        return {status:200 , error:undefined , data:locations.message}
-        // const location = await Location.find(id)
-        // await location.delete()
+    async destroy({ request }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            if (user.status == 'customer') {
+                return 'only admin can access the information'
+            } else {
+                const { id } = request.params
+                const locationUtil = new LocationUtil(Location)
+                const locations = await locationUtil.deleteLocation(id)
+                return { status: 200, error: undefined, data: locations.message }
+            }
+        } catch {
+            return 'only admin can acces the information'
+        }
     }
-
 
 }
 
