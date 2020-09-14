bbb081d5255ae67db4e777b15c8acc9c01e73d61
chubbytoey <toey.sasikrang@gmail.com>
2020-09-14 21:00:15 +0700

    edit customer-validator-test

--- test/unit/customer-validator.spec.js
+++ test/unit/customer-validator.spec.js
@@ -5,26 +5,29 @@ const customerValidator = require('../../service/CustomerValidator')
 
 test('should return undefined error when input correct data', async ({ assert }) => {
   const validatedData = await customerValidator({
-    first_name : "ramita",
-    last_name : "12345678",
-    age : "3",
-    gender : "male"
+    first_name: "ramita",
+    last_name: "12345678",
+    age: "3",
+    gender: "male"
   })
   assert.isOk(validatedData)
 })
 
 test('should return error if input incorrect data', async ({ assert }) => {
   const validatedData = await customerValidator({
-    last_name : "12345678",
-    gender : "male"
+    first_name : "chubby",
+    last_name: "12345678",
+    age : "wow",
+    gender: "male"
   })
   assert.isArray(validatedData.error)
 })
 test('should return only one error if input only one incorrect data', async ({ assert }) => {
   const validatedData = await customerValidator({
-    first_name : "ramita",
-    age : "3",
-    gender : "male"
+    first_name: "ramita",
+    last_name : "saro",
+    age: "3",
+    gender: "male"
   })
-  assert.equal(validatedData.error.length , 1)
+  assert.equal(validatedData.error.length, 1)
 })
