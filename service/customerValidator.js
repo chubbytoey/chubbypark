65125450bc152e0266cc50f4959ee58b7e0eb8db
chubbytoey <toey.sasikrang@gmail.com>
2020-09-14 21:45:04 +0700

    edit customerValidator to have specific rules

--- service/customerValidator.js
+++ service/customerValidator.js
@@ -1,20 +1,20 @@
 const Validator = use('Validator')
 module.exports = async function accountValidator(data) {
-    if(typeof data !== 'object')
+    if (typeof data !== 'object')
         throw new Error()
 
-    const {first_name,last_name,age,gender} = data
-    
+    const { first_name, last_name, age, gender } = data
+
     const rules = {
-        first_name : 'required',
-        last_name : 'required',
-        age : 'required',
-        gender : 'required'
+        first_name: 'required | string | max:120',
+        last_name: 'required | string | max:120',
+        age: 'required | integer | max:3',
+        gender: 'required | string'
     }
-    
-    const validation = await Validator.validateAll({first_name,last_name,age,gender,user_rate:0,previllage:'normal',reservation:0,cancle:0},rules)
+
+    const validation = await Validator.validateAll({ first_name, last_name, age, gender, user_rate: 0, previllage: 'normal', reservation: 0, cancle: 0 }, rules)
 
     return {
-        error:validation.messages() || undefined
+        error: validation.messages() || undefined
     }
 }
