c44845e918d7c9b30e14938f97a113dccee6604c
chubbytoey <toey.sasikrang@gmail.com>
2020-09-14 20:25:09 +0700

    edit customer-validator-test

--- test/unit/customer-validator.spec.js
+++ test/unit/customer-validator.spec.js
@@ -3,33 +3,28 @@
 const { test } = use('Test/Suite')('Customer Validator')
 const customerValidator = require('../../service/CustomerValidator')
 
-test('should receive object as first parametre', async ({ assert }) => {
-  const validatedData = await accountValidator({
-    username : "chubbytoey",
-    password : "12345678"
+test('should return undefined error when input correct data', async ({ assert }) => {
+  const validatedData = await customerValidator({
+    first_name : "ramita",
+    last_name : "12345678",
+    age : "3",
+    gender : "male"
   })
   assert.isOk(validatedData)
 })
 
 test('should return error if input incorrect data', async ({ assert }) => {
-  const validatedData = await accountValidator({
-    username : "chubbytoey",
-    password : "toey"
+  const validatedData = await customerValidator({
+    last_name : "12345678",
+    gender : "male"
   })
   assert.isArray(validatedData.error)
 })
-test('should return only one error if input incorrect password', async ({ assert }) => {
-  const validatedData = await accountValidator({
-    username : "chubbytoey",
-    password : "toey"
+test('should return only one error if input only one incorrect data', async ({ assert }) => {
+  const validatedData = await customerValidator({
+    first_name : "ramita",
+    age : "3",
+    gender : "male"
   })
   assert.equal(validatedData.error.length , 1)
-})
-test('should return error if input not unique username' , async ({assert}) => {
-  const validatedData = await accountValidator({
-    username: "hihi",
-    password : "12345678"
-  })
-  console.log(validatedData.error)
-  assert.deepEqual({field:'username'},{field:'username'})
 })
