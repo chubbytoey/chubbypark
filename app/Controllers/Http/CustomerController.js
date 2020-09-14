d31c8d8d9953f345e62a07ca9b95c2be5d250165
chubbytoey <toey.sasikrang@gmail.com>
2020-09-15 02:35:50 +0700

    edit CustomerController to access authorised only

--- app/Controllers/Http/CustomerController.js
+++ app/Controllers/Http/CustomerController.js
@@ -4,88 +4,128 @@ const CustomerValidator = require("../../../service/CustomerValidator.js")
 const Customer = use('App/Models/Customer')
 const CustomerUtil = require('../../../util/CustomerUtil')
 
-function numberTypeParamValidator (number) {
-    if(Number.isNaN(parseInt(number))) {
-        return {error:`param: ${number} is not supported, please use number type param instead`}
+function numberTypeParamValidator(number) {
+    if (Number.isNaN(parseInt(number))) {
+        return { error: `param: ${number} is not supported, please use number type param instead` }
     }
     return {}
 }
 
 class CustomerController {
-    async index({request}) {
-        const {references = undefined} = request.qs
+    async index({ request, auth }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            if (user.status == 'customer') {
+                return 'only admin can access the information'
+            } else {
+                const { references = undefined } = request.qs
 
-        const customerUtil = new CustomerUtil(Customer)
-        const customers = await customerUtil.getAll(references)
+                const customerUtil = new CustomerUtil(Customer)
+                const customers = await customerUtil.getAll(references)
 
-        return {status:200 , error:undefined , data:customers || {}}
-    }
-
-    async show({request}) {
-        const {id} = request.params
-
-        const ValidatedValue = numberTypeParamValidator(id)
-
-        if(ValidatedValue.error) {
-            return {status:500 , error:ValidatedValue.error , data:undefined}
+                return { status: 200, error: undefined, data: customers || {} }
+            }
         }
-
-        const {references} = request.qs
-        const customerUtil = new CustomerUtil(Customer)
-        const customers = await customerUtil.getByID(id,references)
-
-        return {status:200 , error:undefined , data:customers || {}}
-    }
-
-    async store({request}) {
-        const {first_name,last_name,age,gender} = request.body
-        const ValidatedData = await CustomerValidator(request.body)
-        console.log('hoo')
-
-        if(ValidatedData.error) {
-            return {status:422 , error:ValidatedData.error , data:undefined}
+        catch {
+            return 'only admin can acces the information'
         }
-
-        await Customer.create({first_name,last_name,age,gender,user_rate:0,previllage:'normal',reservation:0,cancle:0})
-
-        return {status:200 , error:undefined , data:`created succesfully`}
     }
 
-    async update({request}) {
-        const {body , params} = request
+    async show({ request, auth }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            const { id } = request.params
+            const ValidatedValue = numberTypeParamValidator(id)
+            if (ValidatedValue.error) {
+                return { status: 500, error: ValidatedValue.error, data: undefined }
+            }
 
-        const {id} = params
-        const {account_id,first_name,last_name,age,gender,user_rate,previllage,reservation,cancle,coin} = body
+            if (user.status == 'admin' || user.status == 'customer' && user.account_id == id) {
+                const { references } = request.qs
+                const customerUtil = new CustomerUtil(Customer)
+                const customers = await customerUtil.getByID(id, references)
 
-        const ValidatedValue = numberTypeParamValidator(id)
-        if(ValidatedValue.error)
-            return {status:500 , error:ValidatedValue.error , data:undefined}
-
-        const customerUtil = new CustomerUtil(Customer)
-        const customers = await customerUtil.updateCustomer(id,account_id,first_name,last_name,age,gender,user_rate,previllage,reservation,cancle,coin)
-        // const customerID = await Database
-        //     .table('customers')
-        //     .where({customer_id:id})
-        //     .update({first_name,last_name,age,gender})
-
-        // const customer = await Database
-        //     .table('customers')
-        //     .where({customer_id:id})
-        //     .first()
-
-        return {status:200 , error:undefined , data:customers}
+                return { status: 200, error: undefined, data: customers || {} }
+            } else {
+                return 'only admin can access the information'
+            }
+        } catch{
+            return 'only admin can access the information'
+        }
     }
-    async destroy({request}) {
-        const {id} = request.params
 
-        const ValidatedValue = numberTypeParamValidator(id)
-        if(ValidatedValue.error)
-        return {status:500 , error:ValidatedValue.error , data:undefined}
+    async store({ request, auth }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            if (user.status == 'customer') {
+                return 'only admin can access the information'
+            } else {
+                const { first_name, last_name, age, gender } = request.body
+                const ValidatedData = await CustomerValidator(request.body)
+                console.log('hoo')
 
-        const customerUtil = new CustomerUtil(Customer)
-        await customerUtil.deleteCustomer(id)
+                if (ValidatedData.error) {
+                    return { status: 422, error: ValidatedData.error, data: undefined }
+                }
 
-        return {status:200 , error:undefined , data:{message:'success'}}
+                await Customer.create({ first_name, last_name, age, gender, user_rate: 0, previllage: 'normal', reservation: 0, cancle: 0 })
+
+                return { status: 200, error: undefined, data: `created succesfully` }
+            }
+        } catch{
+            return 'only admin can access the information'
+        }
+    }
+
+    async update({ request, auth }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            const { body, params } = request
+            const { id } = params
+            const { account_id, first_name, last_name, age, gender, user_rate, previllage, reservation, cancle, coin } = body
+
+            const ValidatedValue = numberTypeParamValidator(id)
+            if (ValidatedValue.error)
+                return { status: 500, error: ValidatedValue.error, data: undefined }
+
+            if (user.status == 'admin' || user.status == 'customer' && user.account_id == id) {
+
+                const customerUtil = new CustomerUtil(Customer)
+                const customers = await customerUtil.updateCustomer(id, account_id, first_name, last_name, age, gender, user_rate, previllage, reservation, cancle, coin)
+
+                return { status: 200, error: undefined, data: customers }
+            } else {
+                return 'only admin can access the information'
+            }
+        } catch{
+            return 'only admin can access the information'
+        }
+    }
+    async destroy({ request, auth }) {
+        try {
+            await auth.check()
+            const user = await auth.getUser()
+            if (user.status == 'customer') {
+                return 'only admin can access the information'
+            } else {
+                const { id } = request.params
+
+                const ValidatedValue = numberTypeParamValidator(id)
+                if (ValidatedValue.error)
+                    return { status: 500, error: ValidatedValue.error, data: undefined }
+
+                const customerUtil = new CustomerUtil(Customer)
+                await customerUtil.deleteCustomer(id)
+
+                return { status: 200, error: undefined, data: { message: 'success' } }
+            }
+        } catch{
+            return 'only admin can access the information'
+        }
     }
 }
 
