2f10b15cd18bc08e546d991ff032ac0b25c92d91
chubbytoey <toey.sasikrang@gmail.com>
2020-09-14 21:45:16 +0700

    edit parkingLotsValidator

--- service/ParkingLotValidator.js
+++ service/ParkingLotValidator.js
@@ -1,5 +1,5 @@
 const Validator = use("Validator")
-module.exports = async function parkinglotValidator (data) {
+module.exports = async function parkinglotValidator(data) {
     if (typeof data !== 'object') throw new Error()
 
     const {
@@ -12,31 +12,28 @@ module.exports = async function parkinglotValidator (data) {
         customer_id
     } = data
 
-
-
     const rules = {
-        lot_name:'required',
-        lot_status:'required',
-        reserve_time:'required',
-        checkin:'required',
-        customer_id:'required',
-        location_id:'required',
-        category_id:'required'
+        lot_name: 'required',
+        lot_status: 'required',
+        reserve_time: 'required',
+        checkin: 'required',
+        customer_id: 'required',
+        location_id: 'required',
+        category_id: 'required'
     }
 
+    const validation = await Validator.validateAll({
+        lot_name,
+        lot_status,
+        reserve_time,
+        checkin,
+        category_id,
+        location_id,
+        customer_id
+    }, rules)
 
-const validation =await Validator.validateAll ({
-    lot_name,
-    lot_status,
-    reserve_time,
-    checkin,
-    category_id,
-    location_id,
-    customer_id
-},rules)
-
-return{
-    error: validation.messages()
+    return {
+        error: validation.messages()
     }
-    
+
 }
