2b15882b1a7f06c622ca668f5d96e43f5397265d
Supphawit-J <ideajeng2543@gmail.com>
2020-09-15 08:14:01 +0700

    edit customer validator

--- service/customerValidator.js
+++ service/customerValidator.js
@@ -12,7 +12,7 @@ module.exports = async function accountValidator(data) {
         gender: 'required | string'
     }
 
-    const validation = await Validator.validateAll({ first_name, last_name, age, gender, user_rate: 0, previllage: 'normal', reservation: 0, cancle: 0 }, rules)
+    const validation = await Validator.validateAll({ first_name, last_name, age, gender, user_rate: 0, previllage: 'normal', reservation: 0, cancel: 0 }, rules)
 
     return {
         error: validation.messages() || undefined
