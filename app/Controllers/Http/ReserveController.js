2b4123bd3df0cc0d2957d3d318a63978e21f3b43
Supphawit-J <ideajeng2543@gmail.com>
2020-10-24 04:52:13 +0700

    edit reserve controller

--- app/Controllers/Http/ReserveController.js
+++ app/Controllers/Http/ReserveController.js
@@ -1,295 +1,336 @@
-'use strict'
+"use strict";
 
-const Location = use('App/Models/Location')
-const ParkingLot = use('App/Models/Parkinglot')
-const Customer = use('App/Models/Customer')
-const Category = use('App/Models/Category')
-const CronJob = require('cron').CronJob;
+const Location = use("App/Models/Location");
+const ParkingLot = use("App/Models/Parkinglot");
+const Customer = use("App/Models/Customer");
+const Category = use("App/Models/Category");
+const CronJob = require("cron").CronJob;
 let timer;
 
 function numberTypeParamValidator(number) {
-    if (Number.isNaN(parseInt(number)))
-        return { error: `param: ${number} is not supported, please use number type param instead.` }
-    return {}
+  if (Number.isNaN(parseInt(number)))
+    return {
+      error: `param: ${number} is not supported, please use number type param instead.`,
+    };
+  return {};
 }
 
 function reserveTimer(userData, lotReserve) {
-    const timeOut = new Date();
-    timeOut.setSeconds(timeOut.getSeconds() + 30)
+  const timeOut = new Date();
+  timeOut.setSeconds(timeOut.getSeconds() + 30);
 
-    let job = new CronJob(timeOut, async function () {
-        console.log(userData)
-        console.log(lotReserve)
+  let job = new CronJob(timeOut, async function () {
+    console.log(userData);
+    console.log(lotReserve);
 
-        lotReserve.merge({ lot_status: "available", reserve_time: null, customer_id: null })
-        await lotReserve.save()
-        userData.merge({ cancel: userData.cancel + 1 })
-        await userData.save()
+    lotReserve.merge({
+      lot_status: "available",
+      reserve_time: null,
+      customer_id: null,
     });
-    return job
+    await lotReserve.save();
+    userData.merge({ cancel: userData.cancel + 1 });
+    await userData.save();
+  });
+  return job;
 }
 
 async function checkToken({ auth }) {
-    try {
-        await auth.check()
-        return true
-    } catch (error) {
-        return { error: 'error' }
-    }
+  try {
+    await auth.check();
+    return true;
+  } catch (error) {
+    return { error: "error" };
+  }
 }
 
 function getFullDate() {
-    const date = new Date()
-    return {
-        year: date.getFullYear(),
-        month: date.getMonth() + 1,
-        day: date.getDate(),
-        hour: date.getHours(),
-        minute: date.getMinutes(),
-        second: date.getSeconds()
-    }
+  const date = new Date();
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    day: date.getDate(),
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds(),
+  };
 }
 
 function timeFormat(timeMilisecond) {
-    let seconds = Math.floor(timeMilisecond / 1000);
-    let minutes = Math.floor(seconds / 60);
-    seconds = seconds % 60;
-    let hours = Math.floor(minutes / 60);
-    minutes = minutes % 60;
-    const time = hours + ":" + minutes + ":" + seconds;
+  let seconds = Math.floor(timeMilisecond / 1000);
+  let minutes = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+  let hours = Math.floor(minutes / 60);
+  minutes = minutes % 60;
+  const time = hours + ":" + minutes + ":" + seconds;
 
-    return time
+  return time;
 }
 
 class ReserveController {
+  async show() {
+    const locations = await Location.query().fetch();
 
-    async show() {
+    return {
+      status: 200,
+      error: undefined,
+      data: locations,
+    };
+  }
+  async showLot({ request, auth }) {
+    const checkLogin = await checkToken(auth);
+    let lots;
+    const { location_id } = request.params;
 
-        const locations = await Location.query()
+    if (checkLogin.error) {
+      return {
+        status: 200,
+        error: undefined,
+        data: await ParkingLot.query()
+          .select("*")
+          .where({ location_id: location_id })
+          .fetch(),
+      };
+    } else {
+      const { references = undefined } = request.qs;
 
-        return {
-            status: 200,
-            error: undefined,
-            data: locations.fetch()
-        }
+      if (references) {
+        const extractedReferences = references.split(",");
+        lots.with(extractedReferences);
+      }
+
+      const token = await auth.getUser();
+      const userData = await Customer.findBy("account_id", token.account_id);
+
+      if (userData.gender == "female" && userData.previllage == "vip") {
+        lots = await ParkingLot.query()
+          .select("*")
+          .where({
+            location_id: location_id,
+            category_id: 1,
+            category_id: 2,
+            category_id: 4,
+          })
+          .fetch();
+      } else if (
+        userData.gender == "female" &&
+        userData.previllage == "disabled"
+      ) {
+        lots = await ParkingLot.query()
+          .where({
+            location_id: location_id,
+            category_id: 1,
+            category_id: 2,
+            category_id: 3,
+          })
+          .fetch();
+      } else if (
+        userData.gender == "female" &&
+        userData.previllage == "normal"
+      ) {
+        lots = await ParkingLot.query()
+          .where({ location_id: location_id, category_id: 1, category_id: 2 })
+          .fetch();
+      } else if (userData.gender == "male" && userData.previllage == "vip") {
+        lots = await ParkingLot.query()
+          .where({ location_id: location_id, category_id: 1, category_id: 4 })
+          .fetch();
+      } else if (
+        userData.gender == "male" &&
+        userData.previllage == "disabled"
+      ) {
+        lots = await ParkingLot.query()
+          .where({ location_id: location_id, category_id: 1, category_id: 3 })
+          .fetch();
+      } else if (userData.gender == "male" && userData.previllage == "normal") {
+        lots = await ParkingLot.query()
+          .where({ location_id: location_id, category_id: 1 })
+          .fetch();
+      }
+      return lots;
     }
-    async showLot({ request, auth }) {
+  }
 
-        const checkLogin = await checkToken(auth)
+  async reserve({ request, auth }) {
+    const checkLogin = await checkToken(auth);
 
-        if (checkLogin.error) {
-            return 'please login'
+    if (checkLogin.error) {
+      return "please login";
+    } else {
+      const { body, params } = request;
+      const { lot_name } = body;
+      const { location_id } = params;
 
-        } else {
+      const token = await auth.getUser();
+      const userData = await Customer.findBy("account_id", token.account_id);
+      const lotReserve = await ParkingLot.findBy({
+        location_id: location_id,
+        lot_name: lot_name,
+      });
 
-            const { location_id } = request.params
+      let date = new Date();
+      let time =
+        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
-            const { references = undefined } = request.qs
-            let lots
+      if (lotReserve.lot_status == "available") {
+        lotReserve.merge({
+          lot_status: "unavailable",
+          customer_id: userData.customer_id,
+          reserve_time: time,
+        });
 
-            if (references) {
-                const extractedReferences = references.split(",")
-                lots.with(extractedReferences)
-            }
+        await lotReserve.save();
 
-            const token = await auth.getUser()
-            const userData = await Customer.findBy('account_id', token.account_id)
+        userData.merge({
+          reservation: userData.reservation + 1,
+        });
+        await userData.save();
 
-            if (userData.gender == "female" && userData.previllage == "vip") {
+        timer = reserveTimer(userData, lotReserve);
+        timer.start();
 
-                lots = await ParkingLot.query().select('*').where({ location_id: location_id, category_id: 1, category_id: 2, category_id: 4 }).fetch()
-
-            } else if (userData.gender == "female" && userData.previllage == "disabled") {
-
-                lots = await ParkingLot.query().where({ location_id: location_id, category_id: 1, category_id: 2, category_id: 3 }).fetch()
-
-            } else if (userData.gender == "female" && userData.previllage == "normal") {
-
-                lots = await ParkingLot.query().where({ location_id: location_id, category_id: 1, category_id: 2 }).fetch()
-
-            } else if (userData.gender == "male" && userData.previllage == "vip") {
-
-                lots = await ParkingLot.query().where({ location_id: location_id, category_id: 1, category_id: 4 }).fetch()
-
-            } else if (userData.gender == "male" && userData.previllage == "disabled") {
-
-                lots = await ParkingLot.query().where({ location_id: location_id, category_id: 1, category_id: 3 }).fetch()
-
-            } else if (userData.gender == "male" && userData.previllage == "normal") {
-
-                lots = await ParkingLot.query().where({ location_id: location_id, category_id: 1 }).fetch()
-
-            }
-            return lots
-        }
-
+        return "success";
+      } else {
+        return "this lot is unavailable";
+      }
     }
+  }
 
-    async reserve({ request, auth }) {
+  async checkin({ auth }) {
+    const checkLogin = await checkToken(auth);
 
-        const checkLogin = await checkToken(auth)
+    if (checkLogin.error) {
+      return "please login";
+    } else {
+      const token = await auth.getUser();
 
-        if (checkLogin.error) {
-            return 'please login'
+      const userData = await Customer.findBy("account_id", token.account_id);
 
-        } else {
+      const lotData = await ParkingLot.findBy(
+        "customer_id",
+        userData.customer_id
+      );
 
-            const { body, params } = request
-            const { lot_name } = body
-            const { location_id } = params
+      let date = new Date();
+      let time =
+        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
+      lotData.merge({ checkin: time });
+      await lotData.save();
 
-            const token = await auth.getUser()
-            const userData = await Customer.findBy('account_id', token.account_id)
-            const lotReserve = await ParkingLot.findBy({ location_id: location_id, lot_name: lot_name })
+      timer.stop();
 
-            let date = new Date();
-            let time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-
-            if (lotReserve.lot_status == "available") {
-
-                lotReserve.merge({
-
-                    lot_status: "unavailable",
-                    customer_id: userData.customer_id,
-                    reserve_time: time
-                })
-
-                await lotReserve.save()
-
-                userData.merge({
-                    reservation: userData.reservation + 1,
-                })
-                await userData.save()
-
-
-
-                timer = reserveTimer(userData, lotReserve)
-                timer.start();
-
-
-
-                return 'success'
-            } else {
-                return 'this lot is unavailable'
-            }
-        }
+      return "check in";
     }
+  }
 
-    async checkin({ auth }) {
+  async checkout({ auth }) {
+    const checkLogin = await checkToken(auth);
 
-        const checkLogin = await checkToken(auth)
+    if (checkLogin.error) {
+      return "please login";
+    } else {
+      const token = await auth.getUser();
 
-        if (checkLogin.error) {
-            return 'please login'
+      const userData = await Customer.findBy("account_id", token.account_id);
+      const lotData = await ParkingLot.findBy(
+        "customer_id",
+        userData.customer_id
+      );
+      const categoryData = await Category.findBy(
+        "category_id",
+        lotData.category_id
+      );
+      const locationData = await Location.findBy(
+        "location_id",
+        lotData.location_id
+      );
 
-        } else {
-            const token = await auth.getUser()
+      const date = getFullDate();
 
-            const userData = await Customer.findBy('account_id', token.account_id)
+      let dateFormat = date.year + "-" + date.month + "-" + date.day;
 
-            const lotData = await ParkingLot.findBy('customer_id', userData.customer_id)
+      console.log(dateFormat);
 
-            let date = new Date();
-            let time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+      let checkOut = new Date(
+        dateFormat + " " + date.hour + ":" + date.minute + ":" + date.second
+      );
+      let checkIn = new Date(dateFormat + " " + lotData.checkin);
 
-            lotData.merge({ checkin: time })
-            await lotData.save()
+      const duration = Date.parse(checkOut) - Date.parse(checkIn);
+      const useHour = timeFormat(duration);
 
-            timer.stop();
+      const paidHour = parseInt(Math.ceil(duration / 3600000));
 
-            return 'check in'
+      if (paidHour > categoryData.free_hour) {
+        let price = paidHour * locationData.price_rate;
 
-        }
+        userData.merge({ coin: userData.coin - price });
 
+        lotData.merge({
+          lot_status: "available",
+          customer_id: null,
+          reserve_time: null,
+          checkin: null,
+        });
+        await lotData.save();
 
+        return (
+          "Check out success , use time : " +
+          useHour +
+          " price : " +
+          price +
+          " remain coin : " +
+          userData.coin
+        );
+      } else {
+        lotData.merge({
+          lot_status: "available",
+          customer_id: null,
+          reserve_time: null,
+          checkin: null,
+        });
+        await lotData.save();
+        return "Check out success , use time : " + useHour + " price : 0";
+      }
     }
+  }
 
-    async checkout({ auth }) {
+  async cancel({ auth }) {
+    const checkLogin = await checkToken(auth);
 
-        const checkLogin = await checkToken(auth)
+    if (checkLogin.error) {
+      return "please login";
+    } else {
+      const token = await auth.getUser();
 
-        if (checkLogin.error) {
+      const userData = await Customer.findBy("account_id", token.account_id);
 
-            return 'please login'
+      const lotData = await ParkingLot.findBy(
+        "customer_id",
+        userData.customer_id
+      );
 
-        } else {
+      lotData.merge({
+        lot_status: "available",
+        customer_id: null,
+        reserve_time: null,
+      });
+      await lotData.save();
 
-            const token = await auth.getUser()
+      userData.merge({
+        cancel: userData.cancel + 1,
+      });
+      await userData.save();
 
-            const userData = await Customer.findBy('account_id', token.account_id)
-            const lotData = await ParkingLot.findBy('customer_id', userData.customer_id)
-            const categoryData = await Category.findBy('category_id', lotData.category_id)
-            const locationData = await Location.findBy('location_id', lotData.location_id)
-
-
-            const date = getFullDate();
-
-            let dateFormat = date.year + "-" + date.month + "-" + date.day;
-
-            console.log(dateFormat)
-
-            let checkOut = new Date(dateFormat + " " + date.hour + ":" + date.minute + ":" + date.second)
-            let checkIn = new Date(dateFormat + " " + lotData.checkin)
-
-            const duration = Date.parse(checkOut) - Date.parse(checkIn);
-            const useHour = timeFormat(duration)
-
-            const paidHour = parseInt(Math.ceil(duration / 3600000))
-
-            if (paidHour > categoryData.free_hour) {
-
-                let price = paidHour * locationData.price_rate;
-
-                userData.merge({ coin: userData.coin - price })
-
-                lotData.merge({ lot_status: "available", customer_id: null, reserve_time: null, checkin: null })
-                await lotData.save()
-
-                return 'Check out success , use time : ' + useHour + ' price : ' + price + ' remain coin : ' + userData.coin
-            } else {
-                lotData.merge({ lot_status: "available", customer_id: null, reserve_time: null, checkin: null })
-                await lotData.save()
-                return 'Check out success , use time : ' + useHour + ' price : 0'
-            }
-        }
+      return "cancelled";
     }
+  }
 
-    async cancel({ auth }) {
-
-        const checkLogin = await checkToken(auth)
-
-        if (checkLogin.error) {
-            return 'please login'
-
-        } else {
-            const token = await auth.getUser()
-
-            const userData = await Customer.findBy('account_id', token.account_id)
-
-            const lotData = await ParkingLot.findBy('customer_id', userData.customer_id)
-
-            lotData.merge({
-                lot_status: "available",
-                customer_id: null,
-                reserve_time: null
-            })
-            await lotData.save()
-
-            userData.merge({
-                cancel: userData.cancel + 1,
-            })
-            await userData.save()
-
-            return 'cancelled'
-        }
-    }
-
-    async test({ auth }) {
-        const date = getFullDate()
-        return date.year
-    }
-
+  async test({ auth }) {
+    const date = getFullDate();
+    return date.year;
+  }
 }
 
-module.exports = ReserveController
+module.exports = ReserveController;
