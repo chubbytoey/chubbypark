edb6449ba069fbd5a67d5036bd1446d13dac12f1
chubbytoey <toey.sasikrang@gmail.com>
2020-10-25 22:29:16 +0700

    edit coinController

--- app/Controllers/Http/CoinController.js
+++ app/Controllers/Http/CoinController.js
@@ -1,18 +1,18 @@
 "use strict";
-const Customer = use("App/Models/Customer");
-const CoinUtil = use("../../../util/CoinUtil");
+const Customer = use('App/Models/Customer');
+const CoinUtil = require('../../../util/CoinUtil');
 
 class CoinController {
-  async updateCoin({ request }) {
+  async updateCoin({ request, auth }) {
     // const { id } = request.params;
-    const { amount } = request.body;
-    await auth.check()
+    const { amount } = request.body
+    await auth.check();
     const user = await auth.getUser()
     const id = user.account_id
     try {
-      const coinUtil = new CoinUtil(Customer);
-      const coins = await coinUtil.updateCoin(id, amount);
-      return { status: 200, error: undefined, data: coins };
+      const coinUtil = new CoinUtil(Customer)
+      const coins = await coinUtil.updateCoin(id, amount)
+      return { status: 200, error: undefined, data: coins.coin }
     } catch {
       return { status: 500, error: "can't update coin", data: undefined };
     }
